ee02c9648a3180f2b5f5f0485e8a27a0954f9672
Vikash <vikashsinghal509@gmail.com>
2024-05-26 14:46:04 +0530

    add on file

---
+++ script.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD46KcHF8Sop9VOCYbOzZLIauMfgukiZ90",
+    authDomain: "skincare-5470d.firebaseapp.com",
+    projectId: "skincare-5470d",
+    storageBucket: "skincare-5470d.appspot.com",
+    messagingSenderId: "1049316262268",
+    appId: "1:1049316262268:web:2acf3823d085b089fde4f9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
