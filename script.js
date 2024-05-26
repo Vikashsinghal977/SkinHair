00f0baacddfec64e2ff27afe1132b4b806463563
Vikash <vikashsinghal509@gmail.com>
2024-05-26 15:34:57 +0530

    connect firebase

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+ import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-database.js";
+ const firebaseConfig = {
     apiKey: "AIzaSyD46KcHF8Sop9VOCYbOzZLIauMfgukiZ90",
     authDomain: "skincare-5470d.firebaseapp.com",
     projectId: "skincare-5470d",
     storageBucket: "skincare-5470d.appspot.com",
     messagingSenderId: "1049316262268",
     appId: "1:1049316262268:web:2acf3823d085b089fde4f9"
-  };
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+document.getElementById("submit").addEventListener('click', function(e){
+    set(ref(db, 'user/' + document.getElementById("username").value),{
+        username:  document.getElementById("username").value,
+        phone:  document.getElementById("phone").value,
+        selectd:  document.getElementById("selectd").value,
+        dob:  document.getElementById("dob").value
+    });
+    alert("Thanks for request");
+})
