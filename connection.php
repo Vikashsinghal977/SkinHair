424439c94f56a774cfcbda9771bb413e9678a13e
Vikash <vikashsinghal509@gmail.com>
2024-05-30 09:28:39 +0530

    connect backend

---
+++ connection.php
@@ -0,0 +1,33 @@
+<?php
+
+$username = filter_input(INPUT_POST, 'username');
+$userphone = filter_input(INPUT_POST, 'userphone');
+$userdob = filter_input(INPUT_POST, 'userdob');
+
+if (!empty($username)){
+    if (!empty($userphone)){
+        if (!empty($userdob)){
+            $host= "localhost";
+            $dbusername = "root";
+            $dbpassword = "";
+            $dbname = "skinheair";
+
+            $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+            if (mysqli_connect_error()){
+                die('connection Error('.mysqli_connect_errno().')'.mysqli_connect_error());
+            }
+            else{
+                $sql = "INSERT INTO persone VALUE('$username', '$userphone', '$userdob')";
+                if ($conn->query($sql)){
+                    echo "new record is inserted succefully";
+                }
+                else{
+                    echo("Error :");
+                }
+                $conn->close();
+            }
+        }
+    }
+}
+
+?>
--- script.js
+++ script.js
@@ -1,24 +1,24 @@
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
- import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-database.js";
- const firebaseConfig = {
-    apiKey: "AIzaSyD46KcHF8Sop9VOCYbOzZLIauMfgukiZ90",
-    authDomain: "skincare-5470d.firebaseapp.com",
-    projectId: "skincare-5470d",
-    storageBucket: "skincare-5470d.appspot.com",
-    messagingSenderId: "1049316262268",
-    appId: "1:1049316262268:web:2acf3823d085b089fde4f9"
-};
+//  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+//  import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-database.js";
+//  const firebaseConfig = {
+//     apiKey: "AIzaSyD46KcHF8Sop9VOCYbOzZLIauMfgukiZ90",
+//     authDomain: "skincare-5470d.firebaseapp.com",
+//     projectId: "skincare-5470d",
+//     storageBucket: "skincare-5470d.appspot.com",
+//     messagingSenderId: "1049316262268",
+//     appId: "1:1049316262268:web:2acf3823d085b089fde4f9"
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-const db = getDatabase(app);
+// const db = getDatabase(app);
 
-document.getElementById("submit").addEventListener('click', function(e){
-    set(ref(db, 'user/' + document.getElementById("username").value),{
-        username:  document.getElementById("username").value,
-        phone:  document.getElementById("phone").value,
-        selectd:  document.getElementById("selectd").value,
-        dob:  document.getElementById("dob").value
-    });
-    alert("Thanks for request");
-})
+// document.getElementById("submit").addEventListener('click', function(e){
+//     set(ref(db, 'user/' + document.getElementById("username").value),{
+//         username:  document.getElementById("username").value,
+//         phone:  document.getElementById("phone").value,
+//         selectd:  document.getElementById("selectd").value,
+//         dob:  document.getElementById("dob").value
+//     });
+//     alert("Thanks for request");
+// })
